19d5942042a0ee56e4cd910510dc8589526174ae
FishnCheaps <pashagaleta97@gmail.com>
2017-09-18 11:16:42 +0300

    Added color

--- glef/GlefModel.hpp
+++ glef/GlefModel.hpp
@@ -18,6 +18,7 @@ public:
 	{
 		glDeleteBuffers(1, &vbo);
 		glDeleteVertexArrays(1, &vao);
+		glDeleteBuffers(1, &color);
 	}
 	void setVertices(std::vector<GLfloat> new_vertices)
 	{
@@ -31,6 +32,14 @@ public:
 			genVBO();
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
 		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), reinterpret_cast<void*>(vertices.data()), GL_STATIC_DRAW); //TO_DO set GL_DYNAMIC_DROW if obj rewriten
+	}
+	void setColor(std::vector<GLfloat> new_color)
+	{
+		if (!is_color_set)
+			genCollor();
+		colors = new_color;
+		glBindBuffer(GL_ARRAY_BUFFER, color);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(GLfloat), reinterpret_cast<void*>(colors.data()), GL_STATIC_DRAW);
 	}
 	/** Proceed this element in every frame. Drowing object
 	@param shaders Shader that should be used for drowing model
@@ -49,11 +58,22 @@ public:
 			0,                  // stride
 			(void*)0            // array buffer offset
 		);
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, color);
+		glVertexAttribPointer(
+			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+			3,                                // size
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
 
 		// Draw the triangle !
 		glDrawArrays(GL_TRIANGLES, 0, vertices.size()); // 3 indices starting at 0 -> 1 triangle
 
 		glDisableVertexAttribArray(0);//TO-DO Ask Alehondrii
+		glDisableVertexAttribArray(1);
 	}
 	void postInit() override
 	{
@@ -71,9 +91,12 @@ private:
 	GLuint vbo;
 	GLuint vao;
 	GLuint matrix;
+	GLuint color;
 	bool is_vbo_set = 0;
 	bool is_vao_set = 0;
+	bool is_color_set = 0;
 	std::vector<GLfloat> vertices;
+	std::vector<GLfloat> colors;
 	/** Generate VAO array and store its handler in vao field
 	*/
 	void genVAO()
@@ -89,5 +112,12 @@ private:
 	{
 		glGenBuffers(1, &vbo);
 		is_vbo_set = true;
+	}
+	/** Generate collor array and store its handler in vbo field
+	*/
+	void genCollor()
+	{
+		glGenBuffers(1, &color);
+		is_color_set = true;
 	}
 };
